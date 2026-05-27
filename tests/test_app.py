1364b55df269c0c139e057408dc90a81c45be988
lenia tamegue <tameguelenia@gmail.com>
2026-05-27 14:01:23 +0000

    Edit test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import json
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'app'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from app import app
 
 @pytest.fixture
