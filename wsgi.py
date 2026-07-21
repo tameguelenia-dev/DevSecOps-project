65d4b44965d9a806154bbf880c599f0e2b3320d7
lenia tamegue <tameguelenia@gmail.com>
2026-07-21 04:29:56 +0000

    Edit wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'app'))
+# Add the app directory to the Python path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app'))
 
 from app import app
 
+application = app
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
